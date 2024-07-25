5d32c6261500c1e3c3f54d656355c7cd3592aede
medi77na <medinastevenurregodavid@gmail.com>
2024-07-25 08:01:25 -0500

    feat: se crea el enum TiposDocumentos.

---
+++ Enums/TipoDocumento.cs
@@ -0,0 +1,12 @@
+namespace entregable_04.Enums
+{
+    public enum TipoDocumento
+    {
+        CC,
+        TI,
+        RC,
+        CE,
+        CI,
+        DNI
+    }
+}
---
+++ Interfaces/IPersona.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace entregable_04.Interfaces
+{
+    public interface IPersona
+    {
+
+
+    }
+}
